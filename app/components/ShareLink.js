ccd2680c3c209c13e52fa2ac90a139b109fc726c
piumosso <golikoffartem@gmail.com>
2020-05-11 22:15:56 +0300

    Выбор ссылки

--- app/components/ShareLink.js
+++ app/components/ShareLink.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import {useSelector} from 'react-redux';
 
 export default () => {
   const game = useSelector(state => state.game);
   const gameLink = game && `${location.origin}/${game.id}/`;
+  const ref = useRef(null);
 
   return <div className="share-link">
     <div className="share-link__label">
       Скопируй ссылку и пригласи друзей в игру
     </div>
-    <input className="share-link__input" type="text" value={gameLink} readOnly={true} />
-  </div>
+    <input className="share-link__input" type="text" value={gameLink} readOnly={true} ref={ref}
+           onClick={() => ref.current.select()}/>
+  </div>;
 };
