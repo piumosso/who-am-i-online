77b8e9cd9be5b6ce53ffb8ab41998d6df54d86cb
piumosso <golikoffartem@gmail.com>
2020-05-11 16:16:57 +0300

    heroku

--- app/components/PersonsMatch.js
+++ app/components/PersonsMatch.js
@@ -6,7 +6,8 @@ export default ({game: {players, matchIndexes}, playerId}) => <section>
     Who is who:
   </div>
   <div>
-    {players.map(({id, name, isFinished}, index) => <div key={index}
+    {players.map(({id, name, isFinished}, index) =>
+      <div key={index}
                                                          style={{textDecoration: isFinished ? 'line-through' : 'none'}}>
       {index + 1}. {name} — {id !== playerId ? players[matchIndexes[index]].person : '?'}
     </div>)}
