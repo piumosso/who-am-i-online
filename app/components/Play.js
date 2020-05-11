dbfefc2c81bb5a34cc98619f8dea72548c1b9efd
piumosso <golikoffartem@gmail.com>
2020-05-11 13:53:24 +0300

    Получение игры

--- app/components/Play.js
+++ app/components/Play.js
@@ -1,6 +1,24 @@
-import React from 'react';
-// import {NavLink} from 'react-router-dom';
+import React, {useEffect} from 'react';
+import {useSelector} from 'react-redux';
+import {useParams} from 'react-router-dom';
+import {store} from '../store';
+import {createPlayer, fetchGame} from '../actions';
 
-export default () => <section className="index">
-  Игра
-</section>;
+export default () => {
+  const params = useParams();
+  const {playerId, inProgress} = useSelector(state => state.app);
+  const game = useSelector(state => state.game);
+
+  useEffect(() => {
+    if (!game || !game.id && !inProgress) {
+      store.dispatch(fetchGame({gameId: params.gameId}))
+    }
+    if (!playerId && !inProgress) {
+      store.dispatch(createPlayer({playerId: params.playerId}))
+    }
+  });
+
+  return <section className="index">
+    Игра
+  </section>;
+}
